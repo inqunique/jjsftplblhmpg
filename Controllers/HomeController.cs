4eea1b61b1c6419c58a017267db0d36428f35a36
zaksim <rubiwind@naver.com>
2017-08-05 20:11:20 +0900

    3

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace PlableHompage.Controllers
+namespace PlableHomepage.Controllers
 {
     public class HomeController : Controller
     {
--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace PlableHompage
+namespace PlableHomepage
 {
     public class Startup
     {
