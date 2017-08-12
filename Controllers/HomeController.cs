421cc4b35d98bb3f7fc51675b19a32f772a41e5e
zaksim <rubiwind@naver.com>
2017-08-13 00:07:22 +0900

    7

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PlableHomepage.Models;
+using MimeKit;
+using MailKit.Net.Smtp;
 
 namespace PlableHomepage.Controllers
 {
@@ -25,10 +28,41 @@ namespace PlableHomepage.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
 
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(MailingModel form)
+        {
+            //var message = new MimeMessage();
+            //message.From.Add(new MailboxAddress("rubiwind", "rubiwind@naver.com"));
+            //message.To.Add(new MailboxAddress("rubiwind", "rubiwind@naver.com"));
+            //message.Subject = "Plable Homepage - Contact";
+            //message.Body = new TextPart("plain")
+            //{
+            //    Text = String.Format("NAME: {0}{3}{3}E-MAIL: {1}{3}{3}MESSAGE: {2}{3}{3}", form.Name, form.Email, form.Text, Environment.NewLine)
+            //};
+
+            //using (var client = new SmtpClient())
+            //{
+            //    client.Connect("smtp.naver.com", 465, true);
+            //    client.Authenticate("rubiwind", "cryspe1708");
+            //    //client.AuthenticationMechanisms.Remove("XOAUTH2");
+            //    // Note: since we don't have an OAuth2 token, disable 	// the XOAUTH2 authentication mechanism.     client.Authenticate("anuraj.p@example.com", "password");
+            //    client.Send(message);
+            //    client.Disconnect(true);
+            //}
+
+            return View();
+        }
+
+        public IActionResult Solution()
+        {
             return View();
         }
 
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,30 @@
 ﻿// Write your Javascript code.
+$('document').ready(function () {
+    if (location.pathname !== '/')
+        $('nav').addClass('navbar-shadow');
+    else
+        $('nav').removeClass('navbar-shadow');
+
+    function selectMenu() {
+        $("a[href='" + location.pathname + "'][class='menu']").addClass('menu-selected');
+    };
+
+    selectMenu();
+
+    $("a[class~='menu']").mouseover(function () {
+        $("a[class~='menu-selected']").removeClass('menu-selected');
+    }).mouseout(function () {
+        selectMenu();
+    });
+
+    $("img[class*='link']").mouseover(function () {
+        $(this).attr('src', $(this).attr('src').slice(0, -5) + "2.png");
+    }).mouseout(function () { $(this).attr('src', $(this).attr('src').slice(0, -5) + "1.png"); });
+
+    //if ($("li[data-target='#itemCarousel']")) {
+    //    $("li[data-target='#itemCarousel'][class='item']").addClass('hide');
+    //}
+
+    $('#itemCarousel').bind('slide.bs.carousel', function (e) {
+    });
+});
